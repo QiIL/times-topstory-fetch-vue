8d1d5a25cad86a62c78d56e440f4ab7a6bdb93a2
QiLL <qiusy@healthmall.cn>
2017-08-10 16:53:26 +0800

    pull master

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+let newYorkApiKey = "4dfd6d88bc06460883571aef03350a5e";
+let baseUrl = "https://api.nytimes.com/svc/topstories/v2/home.json?";
+
+Vue.component('new-view-component',{
+  template: '<div>A custom component</div>'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
